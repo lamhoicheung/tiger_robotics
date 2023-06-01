e42304b3dab53ac07c4f00e4e50b1c3e6ec0a3b0
lukascheung <43694222+lamhoicheung@users.noreply.github.com>
2023-06-02 00:15:09 +0800

    Update elif_elliot.ino

--- esp32/elif_elliot/elif_elliot.ino
+++ esp32/elif_elliot/elif_elliot.ino
@@ -14,8 +14,6 @@
   // C620
   #define RX_PIN 5
   #define TX_PIN 4
-  #define POLLING_RATE_MS 1000
-  static bool driver_installed = false;
 
   const short currentLowerLimit = -1024;
   const short currentUpperLimit = 1024;
@@ -73,7 +71,7 @@ void setup() {
   // C620
     // Initialize configuration structures using macro initializers
   twai_general_config_t g_config = TWAI_GENERAL_CONFIG_DEFAULT((gpio_num_t)TX_PIN, (gpio_num_t)RX_PIN, TWAI_MODE_NORMAL);
-  twai_timing_config_t t_config = TWAI_TIMING_CONFIG_1MBITS();  //Look in the api-reference for other speed sets.
+  twai_timing_config_t t_config = TWAI_TIMING_CONFIG_1MBITS();
   twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
 
     // Install TWAI driver
@@ -91,18 +89,6 @@ void setup() {
     Serial.println("Failed to start driver");
     return;
   }
-
-    // Reconfigure alerts to detect frame receive, Bus-Off error and RX queue full states
-  uint32_t alerts_to_enable = TWAI_ALERT_RX_DATA | TWAI_ALERT_ERR_PASS | TWAI_ALERT_BUS_ERROR | TWAI_ALERT_RX_QUEUE_FULL;
-  if (twai_reconfigure_alerts(alerts_to_enable, NULL) == ESP_OK) {
-    Serial.println("CAN Alerts reconfigured");
-  } else {
-    Serial.println("Failed to reconfigure alerts");
-    return;
-  }
-
-    // TWAI driver is now successfully installed and started
-  driver_installed = true;
 
 
   // CYTRON
@@ -131,40 +117,37 @@ void loop() {
 
 
   // C620
-  if (!driver_installed) {
-    // Driver not installed
-    delay(1000);
-    return;
-  }
-  // Check if alert happened
-  uint32_t alerts_triggered;
-  twai_read_alerts(&alerts_triggered, pdMS_TO_TICKS(POLLING_RATE_MS));
-  twai_status_info_t twaistatus;
-  twai_get_status_info(&twaistatus);
+  short wheelAccel[4] = {800, 800, 800, 800};
+  byte currentBytes[4][2];
 
-  // Handle alerts
-  if (alerts_triggered & TWAI_ALERT_ERR_PASS) {
-    Serial.println("Alert: TWAI controller has become error passive.");
-  }
-  if (alerts_triggered & TWAI_ALERT_BUS_ERROR) {
-    Serial.println("Alert: A (Bit, Stuff, CRC, Form, ACK) error has occurred on the bus.");
-    Serial.printf("Bus error count: %d\n", twaistatus.bus_error_count);
-  }
-  if (alerts_triggered & TWAI_ALERT_RX_QUEUE_FULL) {
-    Serial.println("Alert: The RX queue is full causing a received frame to be lost.");
-    Serial.printf("RX buffered: %d\t", twaistatus.msgs_to_rx);
-    Serial.printf("RX missed: %d\t", twaistatus.rx_missed_count);
-    Serial.printf("RX overrun %d\n", twaistatus.rx_overrun_count);
+    // split into bytes
+  for (int i = 0; i < 4; i++) {
+    byte currentHB = (wheelAccel[i] >> 8) & 0xFF;
+    byte currentLB = wheelAccel[i] & 0xFF;   
+
+    currentBytes[i][0] = currentHB;
+    currentBytes[i][1] = currentLB;
   }
 
-  // Check if message is received
-  if (alerts_triggered & TWAI_ALERT_RX_DATA) {
-    // One or more messages received. Handle all.
-    twai_message_t message;
-    while (twai_receive(&message, 0) == ESP_OK) {
-      handle_rx_message(message);
-      transmit_message();
-    }
+    //Configure message to transmit
+  twai_message_t command;
+  command.identifier = 0x200;
+  command.extd = 0;
+  command.data_length_code = 8;
+  command.data[0] = currentBytes[0][0]; // motor 1
+  command.data[1] = currentBytes[0][1];
+  command.data[2] = currentBytes[1][0]; // motor 2
+  command.data[3] = currentBytes[1][1];
+  command.data[4] = currentBytes[2][0]; // motor 3
+  command.data[5] = currentBytes[2][1];
+  command.data[6] = currentBytes[3][0]; // motor 4
+  command.data[7] = currentBytes[3][1];
+
+  //Queue message for transmission
+  if (twai_transmit(&command, pdMS_TO_TICKS(1000)) == ESP_OK) {
+      Serial.println("Message queued for transmission");
+  } else {
+      Serial.println("Failed to queue message for transmission");
   }
 
 
