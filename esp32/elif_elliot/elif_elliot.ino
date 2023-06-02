ce7d6122d57b904e6a10532e4ebc6a0e9a353c06
lukascheung <43694222+lamhoicheung@users.noreply.github.com>
2023-06-02 11:10:42 +0800

    Update elif_elliot.ino

--- esp32/elif_elliot/elif_elliot.ino
+++ esp32/elif_elliot/elif_elliot.ino
@@ -1,7 +1,7 @@
 // LIBRARIES
 #include <PS4Controller.h>
 #include <ESP32Servo.h>
-#include <CytronMotorDriver.h>
+#include <CytronMotorDriver.h> // need to modify the header or cpp file, comment out ledWrite, directly use analogWrite
 
 
 // PIN DEFINITIONS
@@ -30,7 +30,7 @@
   int liftSpeed = 0;
 
 
-  // STEPPER, take 5V signals
+  // STEPPER, take 5V signals, still has some problem, fail to change direction
   #define STEPPER_DIR 16
   #define STEPPER_PUL 17
   int stepper_speed = 28000;
@@ -104,7 +104,7 @@ void loop() {
   else noTone(STEPPER_PUL);
 
   // BLDC
-  if (PS4.Triangle()) rollerSpeed += 10; // need long delay and small increment otherwise speed changes rapidly, cause motor jerk
+  if (PS4.Triangle()) rollerSpeed += 10; // need long delay and small increment otherwise speed changes rapidly, causing motor jerk
   else if (PS4.Cross()) rollerSpeed -= 10;
 
   rollerSpeed = constrain(rollerSpeed, 0, 500);
