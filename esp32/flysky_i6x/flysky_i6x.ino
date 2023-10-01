b3216f5b51103dc94ec4c613f8aa849762701b2d
lukascheung <43694222+lamhoicheung@users.noreply.github.com>
2023-10-01 08:33:12 +0800

    update

--- esp32/flysky_i6x/flysky_i6x.ino
+++ esp32/flysky_i6x/flysky_i6x.ino
@@ -3,62 +3,75 @@
 
 // Doc: https://github.com/bmellink/IBusBM
 
-
+#include <Arduino.h>
 #include <IBusBM.h>
 
+
 IBusBM ibus;
+const int RC_READ_RATE = 200;
 
 struct Command {
   int left_joy_x;
   int left_joy_y;
   int right_joy_x;
   int right_joy_y;
-  int switch_a;
-  int switch_b;
-  int switch_c;
-  int switch_d;
-  int pot_a;
-  int pot_b;
+  int switch_1;
+  int switch_2;
+  int switch_3;
+  int switch_4;
+  int pot_1;
+  int pot_2;
 };
 
 Command cmd;
 
-void readRC() {
+void taskReadRC(void* pvParam) {
+  ibus.begin(Serial2, 1);  // iBUS object connected to serial2 RX2 pin (pin 16) using timer 1
+  // use Serial1 will crash
 
-  cmd.left_joy_x = ibus.readChannel(3);
-  cmd.left_joy_y = ibus.readChannel(2);
-  cmd.right_joy_x = ibus.readChannel(0);
-  cmd.right_joy_y = ibus.readChannel(1);
-  cmd.switch_a = ibus.readChannel(4);
-  cmd.switch_b = ibus.readChannel(5);
-  cmd.switch_c = ibus.readChannel(6);
-  cmd.switch_d = ibus.readChannel(7);
-  cmd.pot_a = ibus.readChannel(8);
-  cmd.pot_b = ibus.readChannel(9);
+  while (1) {
+    // Value range: 1000-2000
+    cmd.left_joy_x = ibus.readChannel(3);
+    cmd.left_joy_y = ibus.readChannel(2);
+    cmd.right_joy_x = ibus.readChannel(0);
+    cmd.right_joy_y = ibus.readChannel(1);
+    cmd.switch_1 = ibus.readChannel(4);
+    cmd.switch_2 = ibus.readChannel(5);
+    cmd.switch_3 = ibus.readChannel(6);
+    cmd.switch_4 = ibus.readChannel(7);
+    cmd.pot_1 = ibus.readChannel(8);
+    cmd.pot_2 = ibus.readChannel(9);
 
-  Serial.print(cmd.left_joy_x); Serial.print('\t');
-  Serial.print(cmd.left_joy_y); Serial.print('\t');
-  Serial.print(cmd.right_joy_x); Serial.print('\t');
-  Serial.print(cmd.right_joy_y); Serial.print('\t');
-  Serial.print(cmd.switch_a); Serial.print('\t');
-  Serial.print(cmd.switch_b); Serial.print('\t');
-  Serial.print(cmd.switch_c); Serial.print('\t');
-  Serial.print(cmd.switch_d); Serial.print('\t');
-  Serial.print(cmd.pot_a); Serial.print('\t');
-  Serial.print(cmd.pot_b); Serial.println();
+    Serial.print(cmd.left_joy_x);
+    Serial.print('\t');
+    Serial.print(cmd.left_joy_y);
+    Serial.print('\t');
+    Serial.print(cmd.right_joy_x);
+    Serial.print('\t');
+    Serial.print(cmd.right_joy_y);
+    Serial.print('\t');
+    Serial.print(cmd.switch_1);
+    Serial.print('\t');
+    Serial.print(cmd.switch_2);
+    Serial.print('\t');
+    Serial.print(cmd.switch_3);
+    Serial.print('\t');
+    Serial.print(cmd.switch_4);
+    Serial.print('\t');
+    Serial.print(cmd.pot_1);
+    Serial.print('\t');
+    Serial.print(cmd.pot_2);
+    Serial.println();
 
+    vTaskDelay(RC_READ_RATE / portTICK_PERIOD_MS);
+  }
 }
 
+
 void setup() {
-
   Serial.begin(115200);
-  ibus.begin(Serial2, 1);  // iBUS object connected to serial2 RX2 pin (pin 16) using timer 1
-
+  xTaskCreate(taskReadRC, "task read RC", 1500, NULL, 1, NULL);
 }
 
 void loop() {
-
-  readRC();
-  delay(100);
-
 }
